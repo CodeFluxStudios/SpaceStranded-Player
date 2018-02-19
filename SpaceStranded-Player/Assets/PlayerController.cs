aeff0f0af12cba5931b272a8f33265c78beafbad
atalanthus <jonassebast@yahoo.de>
2018-02-19 21:52:03 +0100

    Added FPS Controller 01

---
+++ SpaceStranded-Player/Assets/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+namespace SpaceStranded.Player
+{
+    public class PlayerController : RigidbodyFirstPersonController
+    {
+
+    }
+}
